9d160998f70364207855d321bd110952f0e78105
Megan Fuhr <MeganFuhr@gmail.com>
2022-01-16 07:53:26 -0500

    added core gameplay - score, question count, latinum display. end game next.

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -6,32 +6,53 @@ import CorrectScore from "./CorrectScore";
 import IncorrectScore from "./IncorrectScore";
 
 export default function Question() {
-  const questionToDisplay = createRandomQuestion(wrongAnswers, rightAnswers);
+  const [latinumDisplay, setLatinumDisplay] = useState([]);
+  const [score, setScore] = useState(0);
+  const [questionCount, setQuestionCount] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(
+    createRandomQuestion(wrongAnswers, rightAnswers)
+  );
 
-  const ruleNumber = questionToDisplay.filter((item) => {
+  //get the rule number to display
+  const ruleNumber = currentQuestion.filter((item) => {
     return item.isCorrect === true;
   });
 
-  const [currentQuestion, setCurrentQuestion] = useState(questionToDisplay);
+  const handleAnswer = (answer) => {
+    console.log("Inside handleAnswer function.");
+    //check if question limit of 10 is reached - if so, display score or something. offer retry.
+    if (questionCount > 9) {
+      alert("GAME OVER");
+      setScore(0);
+      setQuestionCount(0);
+    }
+    //update the question count
+    setQuestionCount((prev) => prev + 1);
+
+    //if isCorrect: true, add score and display latinum
+    if (answer.isCorrect) {
+      //update score
+      setScore((prev) => prev + 10);
+      //update latinum display
+      setLatinumDisplay([...latinumDisplay, <CorrectScore />]);
+    } else {
+      //update latinum display
+      setLatinumDisplay([...latinumDisplay, <IncorrectScore />]);
+    }
+    //display new question
+    setCurrentQuestion(createRandomQuestion(wrongAnswers, rightAnswers));
+  };
+
   return (
     <section className="container">
       <div className="question-container">
         <div className="display-score">
-          <h4 className="display-question-count-left">Question 1 of 10</h4>
-          <h4 className="display-score-right">Score: 10</h4>
+          <h4 className="display-question-count-left">
+            Question {questionCount} of 10
+          </h4>
+          <h4 className="display-score-right">Score: {score}</h4>
         </div>
-        <div className="display-latinum">
-          <IncorrectScore />
-          <IncorrectScore />
-          <CorrectScore />
-          <CorrectScore />
-          <IncorrectScore />
-          <CorrectScore />
-          <CorrectScore />
-          <IncorrectScore />
-          <IncorrectScore />
-          <CorrectScore />
-        </div>
+        <div className="display-latinum">{latinumDisplay}</div>
         <div className="question-card">
           <h2>Rule {ruleNumber[0].id}</h2>
           {currentQuestion.map((item) => {
@@ -39,7 +60,7 @@ export default function Question() {
               <button
                 className="question-answer-button"
                 key={item.id}
-                onClick={() => setCurrentQuestion}
+                onClick={() => handleAnswer(item)}
               >
                 {item.answer}
               </button>
